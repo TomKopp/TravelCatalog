85f09bbfccafa0c59e0c7a2d9ce85b2554dac3f6
Tom Kopp <witzbould@live.com>
2019-01-07 18:06:35 +0100

    update MediaList to use key prop

--- src/MediaList.jsx
+++ src/MediaList.jsx
@@ -2,5 +2,5 @@ import MediaObj from './MediaObj';
 import React from 'react';
 
 export default ({ children }) => <ul className="media-list">
-    {children.map((item) => <li className="media-item"><MediaObj {...item} /></li>)}
+    {children.map((item, key) => <li key={key} className="media-item"><MediaObj {...item} /></li>)}
 </ul>;
