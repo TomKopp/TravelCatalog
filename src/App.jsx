d9652dbb7f4d323ae0060da45c28d2565dd5d050
Tom Kopp <witzbould@live.com>
2019-02-05 16:04:24 +0100

    remove unnessesary grid

--- src/App.jsx
+++ src/App.jsx
@@ -62,27 +62,23 @@ export default class App extends Component {
                 </Toolbar>
             </AppBar>
             <div style={{ padding: '16px' }}>
-                <Grid container spacing={16}>
-                    <Grid item className="app-grid-item app-filter">
-                        <Paper style={{ padding: '16px' }}>
-                            <Grid container spacing={16} alignItems="center">
-                                <Grid item style={{ flex: '1 1 100%', minWidth: '17em' }}><TextField label="Destination" name="destination" type="search" margin="normal" variant="outlined" fullWidth onChange={this.handleChange('destination')} /></Grid>
-                                <Grid item style={{ flex: '1 1 50%', minWidth: '17em' }}><TextField label="Arrival" name="arrival" type="date" margin="normal" variant="outlined" fullWidth onChange={this.handleChange('arrival')} InputLabelProps={{ shrink: true }} InputProps={{ startAdornment: <InputAdornment position="start"><Today /></InputAdornment> }} /></Grid>
-                                <Grid item style={{ flex: '1 1 50%', minWidth: '17em' }}><TextField label="Departure" name="departure" type="date" margin="normal" variant="outlined" fullWidth onChange={this.handleChange('departure')} InputLabelProps={{ shrink: true }} InputProps={{ startAdornment: <InputAdornment position="start"><Today /></InputAdornment> }} /></Grid>
-                                <Grid item style={{ flex: '1 1 33.33%' }}><TextField label="Rating" select margin="normal" variant="outlined" fullWidth value={this.state.rating} onChange={this.handleChange('rating')}>
-                                    {this.ratings.map(({ value, label }) => <MenuItem key={value} value={value}>{label}</MenuItem>)}
-                                </TextField></Grid>
-                                <Grid item style={{ flex: '1 1 33.33%' }}><TextField label="Sort By" select margin="normal" variant="outlined" fullWidth value={this.state.sortBy} onChange={this.handleChange('sortBy')}>
-                                    {this.sortBy.map(({ value, label }) => <MenuItem key={value} value={value}>{label}</MenuItem>)}
-                                </TextField></Grid>
-                                <Grid item container justify="flex-end" style={{ flex: '1 1 33.33%' }}><Button color="primary" variant="contained" size="large">Search/Filter</Button></Grid>
-                            </Grid>
-                        </Paper>
+                <Paper className="app-filter">
+                    <Grid container spacing={16} alignItems="center">
+                        <Grid item style={{ flex: '1 1 100%', minWidth: '17em' }}><TextField label="Destination" name="destination" type="search" margin="normal" variant="outlined" fullWidth onChange={this.handleChange('destination')} /></Grid>
+                        <Grid item style={{ flex: '1 1 50%', minWidth: '17em' }}><TextField label="Arrival" name="arrival" type="date" margin="normal" variant="outlined" fullWidth onChange={this.handleChange('arrival')} InputLabelProps={{ shrink: true }} InputProps={{ startAdornment: <InputAdornment position="start"><Today /></InputAdornment> }} /></Grid>
+                        <Grid item style={{ flex: '1 1 50%', minWidth: '17em' }}><TextField label="Departure" name="departure" type="date" margin="normal" variant="outlined" fullWidth onChange={this.handleChange('departure')} InputLabelProps={{ shrink: true }} InputProps={{ startAdornment: <InputAdornment position="start"><Today /></InputAdornment> }} /></Grid>
+                        <Grid item style={{ flex: '1 1 33.33%' }}><TextField label="Rating" select margin="normal" variant="outlined" fullWidth value={this.state.rating} onChange={this.handleChange('rating')}>
+                            {this.ratings.map(({ value, label }) => <MenuItem key={value} value={value}>{label}</MenuItem>)}
+                        </TextField></Grid>
+                        <Grid item style={{ flex: '1 1 33.33%' }}><TextField label="Sort By" select margin="normal" variant="outlined" fullWidth value={this.state.sortBy} onChange={this.handleChange('sortBy')}>
+                            {this.sortBy.map(({ value, label }) => <MenuItem key={value} value={value}>{label}</MenuItem>)}
+                        </TextField></Grid>
+                        <Grid item container justify="flex-end" style={{ flex: '1 1 33.33%' }}><Button color="primary" variant="contained" size="large">Search/Filter</Button></Grid>
                     </Grid>
-                    {this.state.tripList
-                        .filter(this.filterTrip(this.state))
-                        .map((el) => <Grid key={el.id} item className="app-grid-item"><Trip {...el} /></Grid>)}
-                </Grid>
+                </Paper>
+                {this.state.tripList
+                    .filter(this.filterTrip(this.state))
+                    .map((el) => <Trip key={el.id} {...el} />)}
             </div>
             </>;
     }
