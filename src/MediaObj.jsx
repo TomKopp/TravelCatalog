897fb95ae44fa00127234015b6b301e0c13cb99c
Tom Kopp <witzbould@live.com>
2019-02-05 16:05:35 +0100

    break up media list into reusable parts

--- src/MediaObj.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default ({ type, src, description }) => {
-    const Content = type;
-    return <div className="media-obj">
-        <Content className="media" src={src} />
-        <span className="description">{description}</span>
-    </div>;
-};
